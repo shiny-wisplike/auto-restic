0b968a6babb265076868638e00310466eb86a904
shiny-wisplike <287833564+shiny-wisplike@users.noreply.github.com>
2026-09-07 16:10:55 -0400

    Check if the current time matches with the sceduled time in the schedule CSV file

--- check-time.sh
+++ check-time.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROJECT_FOLDER="$(dirname "$(readlink -f "$0")")"
+CSV_FIELDS="csv_day csv_time"
 
 checkDay() {
   IS_MATCH="false"
@@ -20,7 +21,26 @@ checkDay() {
   else
     exit
   fi
+}
 
+checkTime() {
+  IS_MATCH="false"
+  CURRENT_TIME=$(date +"%H:%M")
+
+  while IFS="|" read -r $CSV_FIELDS; do
+    if [[ "$CURRENT_TIME" == "$csv_time" ]]; then
+      echo "Current time matches with scheduled time!"
+      IS_MATCH="true"
+      break
+    fi
+  done < "${PROJECT_FOLDER}/db/schedule.csv"
+
+  if [[ "$IS_MATCH" == "true" ]]; then
+    return
+  else
+    exit
+  fi
 }
 
 checkDay
+checkTime
