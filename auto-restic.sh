4ddc3b42d09ef8b2455c3d75b58c6cd3111e75bd
shiny-wisplike <287833564+shiny-wisplike@users.noreply.github.com>
2026-09-13 21:02:01 -0400

    Added support to exclude certain files and folders when backing up

--- auto-restic.sh
+++ auto-restic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_FOLDER="$(dirname "$(readlink -f "$0")")"
-SCRIPT_VERSION="0.5.0"
+SCRIPT_VERSION="0.6.0"
 BACKUP_REPO=""
 SOURCE_TAG=""
 SOURCE_LOCATION=""
@@ -12,6 +12,7 @@ backup() {
   -r "$BACKUP_REPO" \
   --verbose \
   --password-file "$RESTIC_KEY" \
+  --exclude-file "${PROJECT_FOLDER}/db/excludes.txt" \
   backup \
   --tag "$SOURCE_TAG" \
   "$SOURCE_LOCATION"
